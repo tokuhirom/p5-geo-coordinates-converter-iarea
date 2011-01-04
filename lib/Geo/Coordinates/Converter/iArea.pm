11521af2e0d1f7ac4ad25a50169717846245c628
tokuhirom <tokuhirom@gmail.com>
2011-01-04 12:39:27 +0900

    Checking in changes prior to tagging of version 0.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 41a4fef..0458203 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Geo::Coordinates::Converter::iArea
    
    +0.14
    +
    +    - refactoring
    +
     0.13
    
         - fixed packaging issue

--- lib/Geo/Coordinates/Converter/iArea.pm
+++ lib/Geo/Coordinates/Converter/iArea.pm
@@ -1,7 +1,7 @@
 package Geo::Coordinates::Converter::iArea;
 use strict;
 use warnings;
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 use 5.00800;
 use Geo::Coordinates::Converter;
 use CDB_File;
